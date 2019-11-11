33ba884537f4f28b8d0afc8d7d6ee11ab26de77c
oiuv <i@oiuv.cn>
2019-11-12 01:30:49 +0800

    update Dockerfile

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,14 +4,11 @@ if [ ! -d "/fluffos-build.lock" ]; then
     # Build FluffOS
     cd /opt/docker/fluffos/
     git checkout v2019
-    git reset --hard c8f24e89b3f4d6df77d9bf0bab4aa6c66ce041c2
     rm -rf build/ && mkdir build && cd build
     cmake ..
     make
-    # cargo build -vvv
 
     cp ./src/{driver,portbind} /opt/docker/docker_fluffos/bin/
-    # cp ./target/debug/{fluffos,libdriver.so} /opt/docker/docker_fluffos/bin/
 
     touch /fluffos-build.lock
 fi
